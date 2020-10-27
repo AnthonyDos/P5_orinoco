a8de237daf001b8de7eb671bc3ed0bbe7ea3d24f
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2020-10-27 16:02:03 +0100

    changement de requête

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,19 +1,17 @@
+//section de mes divs
+document.getElementById("produit");
 let cameras = document.getElementsByClassName("cameras");
 
-//Je fais ma requête afin de récupérer le tableau contenant les différentes valeurs des produits.
-let request = new XMLHttpRequest();
-request.onreadystatechange = function() {
-    if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
-        let response = JSON.parse(this.response);
-        console.log(response)
-        //La boucle for me permet d'incrémenter les données a chaque balise section de mon HTML.
-        for (let i = 0; i < 4; i++ ) {         
-            cameras[i].innerHTML = `<img class=imagescamera + src="${response[i].imageUrl}" alt="caméras"/><figcaption id="text_produit"><h2>${response[i].name}</h2><p class="text_produit_p"> Description: ${response[i].description}</p><p id="prix"> Prix: ${response[i].price}€</p><a href="html/produit.html?id=${response[i]._id} class="liens"><button>Aperçu</button></a></figcaption>`
-            cameras[i].setAttribute("data-id", response[i]._id)
-        };
-    };
-};
-
-request.open("GET", "http://localhost:3000/api/cameras", true);
-request.send();
-
+//requête
+fetch('http://localhost:3000/api/cameras')
+  .then(function(response){
+    return response.json()
+  }).then (function (data) {
+    console.log(data)
+    for(let i = 0; i <data.length; i++){
+      cameras[i].innerHTML = `<img class=images + src="${data[i].imageUrl}" alt="caméras"/><figcaption id="text_produit"><h2>${data[i].name}</h2><p class="description"> Description: ${data[i].description}</p><p id="price"> Prix: ${data[i].price}€</p><a href="produit.html?_id=${data[i]._id} class="liens"><button>Aperçu</button></a></figcaption>`
+      cameras[i].setAttribute("data-id", data[i]._id)
+      
+      
+    }
+  })
