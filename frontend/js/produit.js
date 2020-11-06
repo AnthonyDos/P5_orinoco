e2ce35e52cecca8059817f5db522117cd6cabbde
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2020-11-06 23:26:13 +0100

    création de la page produit avec local storage

--- frontend/js/produit.js
+++ frontend/js/produit.js
@@ -4,113 +4,131 @@ let camera2 = document.getElementById('#cameras')
 function getId(){
 	const param = window.location.search
 	const id = param.replace("?id=","") //récupération de l'id et création d'une constante
+	if (!id) throw new Error ('il manque l\'id')
 	return id
 }
 
-
-
 //récupération du produit séléctionner
 fetch ("http://localhost:3000/api/cameras/" + getId())
-.then(response => {
-    return response.json()
+.then(cameras => {
+    return cameras.json()
 })
 .then(function (cameras) {
   console.log(cameras) 
-  //function cameras() {
+//   function cameras() {
 
-  //création des éléments de la page
-                
-  const cameraDiv = document.createElement('div');
-  const cameraTitle = document.createElement('h2');
-  const cameraImage = document.createElement('img');
-  const cameraId = document.createElement('figcaption');
-  const cameraDescription = document.createElement('p');
+	//création des éléments de la page             
+	const cameraDiv = document.createElement('div');
+	const cameraTitle = document.createElement('h2');
+	const cameraImage = document.createElement('img');
+	const cameraId = document.createElement('figcaption');
+	const cameraDescription = document.createElement('p');
 	const optionForm = document.createElement('form');
 	const optionLabel = document.createElement('label');
-  const optionSelect = document.createElement('select');
-	const optionValeur = document.createElement('option');
-	const optionValeur1 = document.createElement('option');
-	const optionValeur2 = document.createElement('option');
-  const buttonDiv = document.createElement('div');
-	const buttonAdd = document.createElement('button');
+	const optionSelect = document.createElement('select');
+	const optionValeur = [];
+	const buttonDiv = document.createElement('div');
+	const buttonAdd = document.createElement('div');
 	const buttonReturn = document.createElement('div');
-  const priceDiv = document.createElement('p');
-  const price = document.createElement('p');
+	const priceDiv = document.createElement('p');
+	const price = document.createElement('p');
 	const cameraQuantity = document.createElement('div');
-  const cameraQuantityText = document.createElement('p');
+	const cameraQuantityText = document.createElement('p');
 	const cameraQuantityInput = document.createElement('input');
 	const divButton = document.createElement('div');
-    
-    
-  //création des noeuds
-  document.querySelector('#cameras').appendChild(cameraDiv);
-  console.log(cameras)
-  cameraDiv.appendChild(cameraTitle);
-  cameraDiv.appendChild(cameraImage);
-  cameraDiv.appendChild(cameraId);
-  cameraDiv.appendChild(cameraDescription);
+	
+	//création des noeuds
+	document.querySelector('#cameras').appendChild(cameraDiv);
+	console.log(cameras)
+	cameraDiv.appendChild(cameraTitle);
+	cameraDiv.appendChild(cameraImage);
+	cameraDiv.appendChild(cameraId);
+	cameraDiv.appendChild(cameraDescription);
 	cameraDiv.appendChild(optionForm);
-	optionForm.appendChild(optionLabel);
-	optionForm.appendChild(optionSelect);
-	optionSelect.appendChild(optionValeur);
-	optionSelect.appendChild(optionValeur1);
-	optionSelect.appendChild(optionValeur2);
-  cameraDiv.appendChild(cameraQuantity);
-  cameraQuantity.appendChild(cameraQuantityText);
-  cameraQuantity.appendChild(cameraQuantityInput);
-  cameraDiv.appendChild(priceDiv);
-  priceDiv.appendChild(price);
+	cameraDiv.appendChild(optionLabel);
+	cameraDiv.appendChild(optionSelect);
+	cameraDiv.appendChild(cameraQuantity);
+	cameraQuantity.appendChild(cameraQuantityText);
+	cameraQuantity.appendChild(cameraQuantityInput);
+	cameraDiv.appendChild(priceDiv);
+	priceDiv.appendChild(price);
 	divButton.appendChild(buttonDiv);
 	buttonDiv.appendChild(buttonReturn);
 	buttonDiv.appendChild(buttonAdd);
 	cameraDiv.appendChild(divButton);
-        
-    
-  //insertion des données
-  cameraTitle.textContent = cameras.name; 
-  cameraImage.src = cameras.imageUrl;
-  cameraQuantityText.textContent = 'Quantité';
+				
+	//j'insers les données
+	cameraTitle.textContent = cameras.name; 
+	cameraImage.src = cameras.imageUrl;
+	cameraQuantityText.textContent = 'Quantité';
 	cameraDescription.textContent = cameras.description;
 	optionLabel.textContent = 'Choix' + ' ';
-  price.textContent = 'Prix:'+ ' '+cameras.price / 100 + '€';
-	buttonAdd.textContent = 'Ajouter'
+	price.textContent = 'Prix:'+ ' '+cameras.price / 100 + '€';
 	buttonReturn.innerHTML =`<button><a  class="liens" href="index.html" >Retour</a></button>` 
-	
-    
-  //attributs
-  cameraImage.setAttribute('alt', 'appareil photo');
-  cameraQuantityInput.setAttribute('type','number');
-  cameraQuantityInput.setAttribute('value',0);
-  cameraQuantityInput.setAttribute('min',0);
+	buttonAdd.innerHTML =`<button><a  class="liens" href="panier.html" >Ajouter</a></button>`
+		
+	//attributs
+	cameraImage.setAttribute('alt', 'appareil photo');
+	cameraQuantityInput.setAttribute('type','number');
+	cameraQuantityInput.setAttribute('value',1);
+	cameraQuantityInput.setAttribute('min',1);
 	buttonAdd.setAttribute('value', 'add');
 	
-    
 	//noms de class
 	cameraDiv.className ='bloc_camera';
 	cameraTitle.className = 'bloc_camera_title'
 	cameraImage.className = 'camera_photo';
 	cameraDescription.className ='bloc_camera_description'
-  cameraQuantity.className = 'Quantity';
-  cameraQuantityInput.className = 'quantity_input';
+	cameraQuantity.className = 'quantity';
+	cameraQuantityInput.className = 'quantity_input';
 	price.className = 'price';
-  buttonDiv.className = 'button';
-  buttonAdd.className = 'button_add';
-    
-  
-    
-  //bouton ajout de l'article au panier pas encore fonctionnel
-  buttonAdd.addEventListener('click', () =>{addTo});
-    
-  //récupérer les données du tableau lenses
-  for (let i = 0; i  <cameras.lenses.length; i++) {
+	buttonDiv.className = 'button';
+	buttonAdd.className = 'button_add';
+	
+	//récupérer les données du tableau lenses
+	for (let i = 0; i  <cameras.lenses.length; i = i + 1) {
 		console.log(cameras.lenses[i])
-      //création du noeud
-		optionSelect.appendChild(optionValeur)
-  	//récupération des données et insertion
-		optionValeur.textContent = cameras.lenses[0];
-		optionValeur1.textContent = cameras.lenses[1];
-		optionValeur2.textContent = cameras.lenses[2];
+
+		//envoi des données dans option 
+		optionValeur.push(document.createElement('option'))
+
+		//création du noeud optionValeur à optionSelect
+		optionSelect.appendChild(optionValeur[i])
+
+		//récupération des données et insertion
+		optionValeur[i].innerHTML = cameras.lenses[i];
 		console.log(cameras.lenses)	
-  }
-})        
-   
+		
+		//j'attibue les données valeur = donnée récupéré dans la boucle au dessus
+		optionSelect.setAttribute('value',cameras.lenses[i])
+		optionSelect[i].id = ('value', cameras.lenses.value) 
+		
+		//ajout au panier
+		buttonAdd.onclick =
+			function  (){
+				//j'impose une condition de sélectionner une quantité
+				if( cameraQuantityInput.value == 0 ){
+					alert('Veuillez sélectionner une quantité');
+				}else{
+				//si la condition est respectée 
+				price.textContent = ((cameras.price / 100) * (+cameraQuantityInput.value)) + '€'
+				let camerasOption = {
+					id : cameras._id,
+					name : cameras.name,
+					price : cameras.price/100,
+					description : cameras.description,
+					image : cameras.imageUrl,
+					optionSelect: cameras.lenses,	
+				}
+				//affiche dans le local storage
+				let objectifOption = JSON.stringify(camerasOption);
+				localStorage.setItem(cameras._id, objectifOption);
+				const lenses = document.getElementsByTagName('select')
+		
+				//let buttonAdd = JSON.parse(localStorage.getItem('cameras'))
+				//alert("ajouté au panier")
+				alert (cameraQuantityInput.value + " " + cameras.name + ' ' + optionSelect.value + ' ajouté au panier')
+			}	
+		}
+	}		
+}).catch(error => console.log(error))
