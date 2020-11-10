e595a8ab5775aa0e64416e2f9980801e3ae3f19e
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2020-11-10 15:04:30 +0100

    ajout de la suppresion d'article

--- frontend/js/panier.js
+++ frontend/js/panier.js
@@ -1,22 +1,25 @@
 let basket = document.getElementById("camerasCard");
-let totalP = document.getElementById("total_p")
+let totalP = document.getElementById("total_p");
+let mainBasket = document.getElementById("main_basket")
 
 //push id de la cameras dans le tableau products
 let productsId = [];
 let totalBasket = 0 ;
 
-// const basketEmpty = document.createElement("div")
-// basket.appendChild(basketEmpty);
-// basketEmpty.innerHTML = "Panier vide";
-
 // //récupération des données stocker dans le localStorage
 let cameraBasket = Object.keys(localStorage) 
-
-for (i=0; i < cameraBasket.length; i++) {
-
+if (cameraBasket.length == 0) {
+    let basketEmpty = document.createElement("p");
+    mainBasket.appendChild(basketEmpty);
+    basketEmpty.className = "basket_empty"
+    basketEmpty.innerHTML =`Votre panier est vide. <i class="far fa-frown"></i>`
+}else{
+    for (i=0; i < cameraBasket.length; i++) {
+        
+        
         let cameraStorage = JSON.parse(localStorage.getItem(cameraBasket[i]))
         let totalPrice = document.createElement("p");      
-
+        ///////////////////////////////////////////
         //création des éléments de la page
         let divContainerPanier = document.createElement("div");
         let divText = document.createElement('div');
@@ -27,7 +30,7 @@ for (i=0; i < cameraBasket.length; i++) {
         let quantity = document.createElement("p");
         let totalProduct = document.createElement("p");
         let cameraText = document.createElement("div")
-
+        ////////////////////////////////////////
         //création des class
         divContainerPanier.className = "container_basket";
         divText.className = "container_basket_text";
@@ -38,9 +41,9 @@ for (i=0; i < cameraBasket.length; i++) {
         quantity.className = "container_basket_quantity";
         totalProduct.className = "container_basket_total_product";
         cameraText.className = "container_basket_camera_text";
+        ////////////////////////////////////////
         //création des noeuds
-        //basket.appendChild(basketEmpty);
-        basket.appendChild(divText);
+         basket.appendChild(divText);
         divText.appendChild(imgCamera)
         cameraText.appendChild(priceBasket)
         divText.appendChild(titleCamera);
@@ -48,7 +51,7 @@ for (i=0; i < cameraBasket.length; i++) {
         cameraText.appendChild(priceProductObjectif);
         cameraText.appendChild(quantity);
         cameraText.appendChild(totalProduct);
-
+        ////////////////////////////////////////
         //attribution des données
         imgCamera.setAttribute('src', cameraStorage.imageUrl)
         titleCamera.innerHTML = cameraStorage.name;
@@ -56,21 +59,23 @@ for (i=0; i < cameraBasket.length; i++) {
         console.log(priceProductObjectif)
         quantity.innerHTML = "Quantité :"+ " "+ cameraStorage.qty  ;
         console.log(quantity)
-        totalProduct.innerHTML = "Prix "+" "+cameraStorage.name+" " +":"+" "+(cameraStorage.price * cameraStorage.qty )+ " "+"€"
+        totalProduct.innerHTML = "Prix "+" " +":"+" "+(cameraStorage.price * cameraStorage.qty ) +" "+"€"
         totalBasket = totalBasket + (cameraStorage.price * cameraStorage.qty )
         console.log(totalBasket)
         productsId.push(cameraStorage.id);
-
+    }
 }
-
-
-// document.getElementById('remove_basket').addEventListener('click', () => {
-//     localStorage.clear();
-//     location.reload();
-// })  
         
+//////////////////////////////////////////////
+//bouton supprimer les articles
+document.getElementById('remove_basket').addEventListener('click', () => {
+     localStorage.clear(); //efface
+    location.reload(); //recharge la page
+ })  
+//////////////////////////////////////////////     
 //afficher le prix total
 let totalPrice = document.createElement("p");
+totalPrice.className = 'total_price';
 totalPrice.innerHTML = "Prix total de votre panier :" + " " +  totalBasket +" "+ "€";
 totalP.appendChild(totalPrice)
 console.log(totalPrice)      
@@ -79,12 +84,38 @@ console.log(totalPrice)
 
 let returnButton = document.getElementById('return_button')
 returnButton.innerHTML =`<button><a  class="liens" href="index.html" >Retour</a></button>` 
-btn_send.innerHTML = `<a class='lien' href="confirmation_de_commande.html"></a>`
+btn_send.innerHTML = `<button><a class='lien' href="confirmation_de_commande.html"></a></button>`
 
 
 
 //Formulaire client
+function sendOrder(){
+    const name = document.getElementById("last_name").value;
+    const firstname = document.getElementById("first_name").value;
+    const mail = document.getElementById("adress").value;
+    const adress = document.getElementById("city").value;
+    const city = document.getElementById("mail").value;  
 
+    const formInformation = new infoForm (name, firstname, mail, adress, city);
+    const basketContent = JSON.parse(localStorage.getItem("cameraBasket"));
 
+    let idOrder = [];
+    
+    for (let i = 0; i < cameraBasket.length; i =  i + 1){
+        cameraBasket[i].id;
+        idOrder.push(cameraBasket[i].id);
+    }
+    const command = new orderInfo(formInformation, idOrder);
+    post("http://localhost:3000/api/cameras/order", command).then( function(response){
+        localStorage.setItem("camerabasket", JSON.stringify([])); 
+        localStorage.setItem("orderConfirmation", response.orderId);
+        window.location.href = "confirmation_de_commande.html"; // on va à la page de confirmation
+    }).catch(function(err){
+        console.log(err);
+        if(err === 0){ // requete ajax annulée
+            alert("serveur HS");
+        }
+    });
+}
 
 
