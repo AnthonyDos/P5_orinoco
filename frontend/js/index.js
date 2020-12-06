8beae4be083ec57e86b34310e6caed66f918c89f
AnthonyDos <47761819+AnthonyDos@users.noreply.github.com>
2020-12-06 23:26:52 +0100

    modif du code de la page index

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,26 +1,106 @@
-//section de mes divs
-document.getElementById("produit");
-let cameras = document.getElementsByClassName("cameras");
+//section divs , je récupére la div pour incorporer mes éléments de la caméra
+let container = document.getElementById("produit");
 
-//requête
+
+//requête fetch
 fetch('http://localhost:3000/api/cameras') 
-  .then(function(response){
+  .then(function(response){                   //je récupére les données de l'api
     return response.json()
   }).then (function (data) {
-  console.log(data)
-  for(let i = 0; i <data.length; i++){
-    cameras[i].innerHTML = `
-    <img class=images + src="${data[i].imageUrl}" alt="caméras"/>
-    <figcaption id="text_produit">
-      <h2>${data[i].name}</h2>
-      <p class="description"> Description: ${data[i].description}</p>
-      <p id="price"> Prix: ${data[i].price/100}€</p>
-      <button><a  class="liens" href="produit.html?id=${data[i]._id}" >Aperçu</a></button>
-    </figcaption>`
-    cameras[i].setAttribute("data-id", data[i]._id) 
-    
-  }        
+  //console.log(data)
+  for(let i = 0; i <data.length; i++){         // je fais une boucle pour récupéré toutes les caméras du tableau
+    const cameras = document.createElement('div');
+    container.appendChild(cameras);
+    cameras.className = ('cameras')
+
+    function afficherLesImages(camerasInfo){
+      const imagesCam = document.createElement('img');
+      cameras.appendChild(imagesCam);
+      imagesCam.src = `${camerasInfo[i].imageUrl} `;
+      imagesCam.className = 'images';
+      imagesCam.setAttribute = alt='caméras'
+      console.log(imagesCam)
+    }
+    afficherLesImages(data)
+
+    const textProducts = document.createElement('div');
+    cameras.appendChild(textProducts);
+    textProducts.className = 'text_produit';
+
+    function afficherLesNoms(camerasInfo){
+      const name = document.createElement('h2');
+      textProducts.appendChild(name);
+      name.textContent = camerasInfo[i].name;
+    }
+    afficherLesNoms(data)
+
+    function descriptionCameras(camerasInfo){
+      const infoCamera = document.createElement('p');
+      textProducts.appendChild(infoCamera);
+      infoCamera.textContent = camerasInfo[i].description;
+      infoCamera.className = 'description'
+    }
+    descriptionCameras(data);
+
+    function priceCamera(camerasInfo){
+      const price = document.createElement('p');
+      textProducts.appendChild(price);
+      price.textContent = `${camerasInfo[i].price/100} €`;
+      price.className = 'price';  
+    }
+    priceCamera(data);
+
+    function buttonId (camerasInfo){
+      const buttonInfo = document.createElement('button');
+      textProducts.appendChild(buttonInfo);
+      buttonInfo.className = "button";
+      buttonInfo.innerHTML = `<a  class="liens" href="produit.html?id=${data[i]._id}" >Aperçu</a>`
+    }
+    buttonId(data)  
+  }       
 }) 
-.catch(function(err) {
-  console.log('Fetch Error ', error);
+.catch(function(error) {
+  console.log(error)
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
